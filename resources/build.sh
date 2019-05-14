af720ee3ab3d5224aa85b58f842c40e19a254809
Chenxi Yuan <yuanchenxi95@gmail.com>
2019-05-14 14:23:41 +0800

    add gh-pages and modify build.sh script

--- resources/build.sh
+++ resources/build.sh
@@ -9,12 +9,13 @@ if [ ! -d "node_modules/.bin" ]; then
 fi
 
 rm -rf dist/ && mkdir -p dist/
+rm -rf build/ && mkdir -p build/
 babel src --ignore __tests__ --out-dir dist/
 echo "Bundling graphiql.js..."
-browserify -g browserify-shim -s GraphiQL dist/index.js > graphiql.js
+browserify -g browserify-shim -s GraphiQL dist/index.js > build/graphiql.js
 echo "Bundling graphiql.min.js..."
-browserify -g browserify-shim -t uglifyify -s GraphiQL dist/index.js 2> /dev/null | uglifyjs -c > graphiql.min.js 2> /dev/null
+browserify -g browserify-shim -t uglifyify -s GraphiQL dist/index.js 2> /dev/null | uglifyjs -c > build/graphiql.min.js 2> /dev/null
 echo "Bundling graphiql.css..."
 postcss --no-map --use autoprefixer -d dist/ css/*.css
-cat dist/*.css > graphiql.css
+cat dist/*.css > build/graphiql.css
 echo "Done"
